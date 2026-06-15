16d13c18e3a0c957f5ed24c481a0b8d4af28542e
Li Kai <1353272517@qq.com>
2026-06-15 15:58:13 +0800

    feat(core): add network gameplay architecture
    Add the Variant_Network gameplay framework for the #10 core architecture milestone, including C++ GameMode, GameState, PlayerState, PlayerController, Character classes, related Blueprint assets, and the network greybox map setup.
    
    Refs #10

---
+++ Source/Arena/Variant_Network/NetGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a first person game
+ */
+UCLASS(abstract)
+class ANetGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANetGameMode();
+};
